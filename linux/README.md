c741c2ef931c176dfad3f721df414918170158ed
jason kososki <jason.kososki@gmail.com>
2021-09-26 04:53:51 -0500

    Adds setup

---
+++ linux/README.md
@@ -0,0 +1,5 @@
+# Linux Dotfiles
+
+Configures dotfiles for a new host and preps the deployment of additional tools.
+
+
---
+++ linux/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+git clone --bare -b linux git@github.com:jkososki/dotfiles.git $HOME/.dotfiles
+
+function dotfiles {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+bkdir="$HOME/.dotfiles-backup-$(date +%s)"
+mkdir -p $bkdir 
+
+dotfiles checkout
+if [ $? = 0 ]; then
+  echo "Checked out dotfiles.";
+  else
+    echo "Backing up pre-existing dot files.";
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $bkdir/{}
+fi;
+
+dotfiles checkout
+dotfiles config status.showUntrackedFiles no
